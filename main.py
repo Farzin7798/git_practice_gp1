08ca4169ff1825d753d8c9c0c4385fc68ffba83a
AmirMohammad Kheradyar <AMKheradyar@gmail.com>
2023-12-31 20:18:04 +0330

    Add pow_of_two_numbers function.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#AmirMohammad
+def pow_of_two_numbers(first_num: int, second_num: int) -> int:
+    if second_num < 0:
+        raise ValueError("Exponent cannot be negative.")
+    if second_num == 0:
+        return 1
+    else:
+        return first_num * pow_of_two_numbers(first_num, second_num - 1)
+
+#Example
+print(pow_of_two_numbers(2, 3))
+
 # Parnian
 
 
