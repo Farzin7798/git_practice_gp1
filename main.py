c97b4ed504051581fc8eb4adc254e978b0ea35f9
AmirMohammad Kheradyar <AMKheradyar@gmail.com>
2023-12-31 19:58:47 +0330

    Initial commit.

---
+++ main.py
@@ -0,0 +1,33 @@
+# Parnian
+
+
+# def remain_of_two_numbers():
+#     pass
+
+
+# Mohammad
+
+
+# def sum_of_two_numbers():
+#     pass
+
+
+# Mehdi
+
+
+# def sub_of_two_numbers():
+#     pass
+
+
+# Farzin
+
+
+# def div_of_two_numbers():
+#     pass
+
+
+# Parsa
+
+
+# def multi_of_two_numbers():
+#     pass
